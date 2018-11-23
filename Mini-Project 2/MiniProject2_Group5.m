6efe2af6628be8f1fa59855613fb84640bd270e0
Gabriele Gambardella <gabriele.gambardella@epfl.ch>
2018-11-23 19:14:06 +0100

    Linear regression added to the code

--- Mini-Project 2/MiniProject2_Group5.m
+++ Mini-Project 2/MiniProject2_Group5.m
@@ -10,9 +10,25 @@ trainData = Data(1:rowsTrain,:);
 testData = Data(rowsTrain+1:end,:);
 
 [trainDataNorm,mu,sigma] = zscore(trainData);
-[coeff,score,~,~,explained] = pca(trainDataNorm);
+[coeff,trainDatascore,~,~,explained] = pca(trainDataNorm);
 testDataNorm = (testData - mu)./sigma;
 testDatascore = testDataNorm*coeff;
 cumulative = cumsum(explained)/sum(explained);
 
 index = find(cumulative>0.9,1);
+
+%% Regressor Training
+
+chosenFeatures = 1:size(trainDatascore,2);
+ytrain = PosX(1:rowsTrain);
+ytest = PosX(rowsTrain+1:end);
+Itrain = ones(size(ytrain,1),1);
+Itest = ones(size(ytest,1),1);
+FMtrain = trainData(:,chosenFeatures);
+FMtest = testData(:,chosenFeatures);
+Xtrain = [Itrain FMtrain];
+Xtest = [Itest FMtest];
+
+b = regress(ytrain,Xtrain);
+trainErr = immse(ytrain,Xtrain*b);
+testErr = immse(ytest,Xtest*b);
